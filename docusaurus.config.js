faf7901697d81cd2d1ba733fd79153156b297e10
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-04-07 16:54:01 +0800

    完成第五章程序优化
    Signed-off-by: ziqianzhang <zhangziqian@sjtu.edu.cn>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,8 +9,8 @@ const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'ICS 课程笔记',
+  tagline: 'ICS is important!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
