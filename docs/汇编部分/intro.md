7ce3617112bdf373d6f6fee1ac07f28368a12e42
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-04-15 18:56:37 +0800

    增加ppt课件

--- docs/汇编部分/intro.md
+++ docs/汇编部分/intro.md
@@ -1,2 +1,7 @@
 
-内容建设中
+内容建设中123
+
+import OfficePreview from '@site/src/components/OfficePreview/index';
+
+
+<OfficePreview place = "/ppt/3-8-rio.ppt"/>
---
+++ src/components/OfficePreview/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
+export default function OfficePreview(props) {
+    return (
+        <BrowserOnly>
+        { () => {
+            return <iframe src={"//view.officeapps.live.com/op/embed.aspx?src=" + window.location.protocol + "//" + window.location.host + props.place + "&amp;wdAr=1.7777777777777777"} >这是嵌入 <a target="_blank" href="https://office.com">Microsoft Office</a> 演示文稿，由 <a target="_blank" href="https://office.com/webapps">Office</a> 提供支持。</iframe>
+        }}
+        </BrowserOnly>
+    );
+}
+
+// frameBorder="0"
