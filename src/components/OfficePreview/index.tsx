5a6811ebd3dc4e8cd8fd0757d10d404c7e1c8fab
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-04-15 19:46:57 +0800

    fix op

--- src/components/OfficePreview/index.tsx
+++ src/components/OfficePreview/index.tsx
@@ -5,7 +5,7 @@ export default function OfficePreview(props) {
     return (
         <BrowserOnly>
         { () => {
-            return <iframe src={"//view.officeapps.live.com/op/embed.aspx?src=" + window.location.protocol + "//" + window.location.host + props.place} >这是嵌入 <a target="_blank" href="https://office.com">Microsoft Office</a> 演示文稿，由 <a target="_blank" href="https://office.com/webapps">Office</a> 提供支持。</iframe>
+            return <iframe src={"//view.officeapps.live.com/op/view.aspx?src=" + window.location.protocol + "//" + window.location.host + props.place} >这是嵌入 <a target="_blank" href="https://office.com">Microsoft Office</a> 演示文稿，由 <a target="_blank" href="https://office.com/webapps">Office</a> 提供支持。</iframe>
         }}
         </BrowserOnly>
     );
